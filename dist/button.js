b8f924680e0d7e103bed320e29ea7683a43e927c
Ross Leitch <ross@hexhive.io>
2022-08-09 14:22:31 +1200

    Button

---
+++ dist/button.js
@@ -0,0 +1,34 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Button = void 0;
+const react_1 = __importDefault(require("react"));
+const material_1 = require("@mui/material");
+const Button = (props) => {
+    return (react_1.default.createElement(material_1.Button, { variant: "contained", color: "primary", sx: {
+            // background: props.color,
+            border: 0,
+            borderRadius: '0.375em',
+            color: props.textColor,
+            cursor: 'pointer',
+            margin: '1.5em auto',
+            padding: '0.5em 1em',
+            textShadow: `0 0 0.1em white`,
+            transition: `box-shadow 0.2s linear`,
+            boxShadow: `
+                    inset 0 -2px 0 rgba(200, 200, 200, 1)
+                `,
+            '&:hover': {
+            // opacity: '0.8'
+            },
+            '&:active': {
+                boxShadow: `unset`
+            }
+        } },
+        react_1.default.createElement("span", { style: {
+                display: 'block'
+            }, tabIndex: -1 }, props.children)));
+};
+exports.Button = Button;
---
+++ src/button.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { Button as ButtonUnstyled } from '@mui/material'
+
+export interface ButtonProps {
+    children?: any;
+    color?: any;
+    textColor?: any;
+}
+
+export const Button : React.FC<ButtonProps> = (props) => {
+    return (
+        <ButtonUnstyled
+            variant="contained"
+            color="primary"
+            sx={{
+                // background: props.color,
+                border: 0,
+                borderRadius: '0.375em',
+                color: props.textColor,
+                cursor: 'pointer',
+                margin: '1.5em auto',
+                padding: '0.5em 1em',
+                textShadow: `0 0 0.1em white`,
+                transition: `box-shadow 0.2s linear`,
+                boxShadow: `
+                    inset 0 -2px 0 rgba(200, 200, 200, 1)
+                `,
+                '&:hover': {
+                    // opacity: '0.8'
+                },
+                '&:active': {
+                    boxShadow: `unset`
+                }
+            }}
+            >
+            <span 
+                style={{
+                    display: 'block'
+                }}
+                tabIndex={-1}>{props.children}</span>
+        </ButtonUnstyled>
+    )
+}
