89e42c6073ceb7c2e6f6ec53a313ec40c62f6774
Ross Leitch <ross@hexhive.io>
2022-09-13 12:41:58 +1200

    Add grid, image, text

--- dist/Grid.js
+++ dist/Grid.js
@@ -6,13 +6,38 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.Grid = void 0;
 const react_1 = __importDefault(require("react"));
 const material_1 = require("@mui/material");
+/*
+    props
+    - editing : bool
+*/
 const Grid = (props) => {
-    return (react_1.default.createElement(material_1.Grid, { container: true, spacing: 2 }, props.items.map((item) => (react_1.default.createElement(material_1.Grid, { item: true, xs: props.cellSize }, item)))));
+    const renderItem = (row, col) => {
+        var _a, _b;
+        if (props.editing) {
+            return (react_1.default.createElement("div", { onDrop: (evt) => {
+                    let data = JSON.parse(evt.dataTransfer.getData('infinite-canvas'));
+                    console.log("Data from grid cell", { row, col });
+                    // let pos = getRelativeCanvasPos(canvasRef, {offset: _offset, zoom: _zoom}, {x: e.clientX, y: e.clientY})
+                    // onNodeCreate(pos, data)
+                    // isDragging.current.dragging = false
+                }, style: { border: '1px solid black' } }, (_a = props.items) === null || _a === void 0 ? void 0 : _a.find((a) => a.row == row && a.col == col)));
+        }
+        else {
+            return (_b = props.items) === null || _b === void 0 ? void 0 : _b.find((a) => a.row == row && a.col == col);
+        }
+    };
+    return (react_1.default.createElement(material_1.Grid, { container: true, spacing: 2 }, Array.from(new Array(props.rows * props.cols)).map((x, ix) => {
+        let row = Math.floor(ix / props.cols);
+        let col = ix % props.cols;
+        return (react_1.default.createElement(material_1.Grid, { item: true, xs: props.cellSize }, renderItem(row, col)));
+    })));
 };
 exports.Grid = Grid;
 exports.Grid.metadata = {
     options: {
         cellSize: 'Number',
+        rows: 'Number',
+        cols: 'Number',
         items: 'Array'
     }
 };
--- src/Grid.tsx
+++ src/Grid.tsx
@@ -1,14 +1,46 @@
 import React from "react";
 import { Grid as MuiGrid } from '@mui/material'
 
+/*
+    props
+    - editing : bool
+*/
 export const Grid = (props: any) => {
+
+    const renderItem = (row: number, col: number) => {
+        if(props.editing){
+            return (
+                <div 
+                    onDrop={(evt) => {
+                        let data = JSON.parse(evt.dataTransfer.getData('infinite-canvas'))
+                       
+                        console.log("Data from grid cell", {row, col});
+                        // let pos = getRelativeCanvasPos(canvasRef, {offset: _offset, zoom: _zoom}, {x: e.clientX, y: e.clientY})
+                        // onNodeCreate(pos, data)
+                        // isDragging.current.dragging = false
+                    }}
+                    style={{border: '1px solid black'}}>
+                    {props.items?.find((a: any) => a.row == row && a.col == col)}
+                </div>
+            )
+        }else{
+            return props.items?.find((a: any) => a.row == row && a.col == col);
+        }
+    }
+
+    
     return (
         <MuiGrid container spacing={2}>
-            {props.items.map((item: any) => (
-                <MuiGrid item xs={props.cellSize}>
-                    {item}
-                </MuiGrid>
-            ))}
+            {Array.from(new Array(props.rows * props.cols)).map((x, ix) => {
+                let row = Math.floor(ix / props.cols);
+                let col = ix % props.cols;
+
+                return (
+                    <MuiGrid item xs={props.cellSize}>
+                        {renderItem(row, col)}
+                    </MuiGrid>
+                )
+            })}
         </MuiGrid>
     )
 }
@@ -16,6 +48,8 @@ export const Grid = (props: any) => {
 Grid.metadata = {
     options: {
         cellSize: 'Number',
+        rows: 'Number',
+        cols: 'Number',
         items: 'Array'
     }
 }
