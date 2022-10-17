0d3fa56883fb9a2e1cc1213da7eaafcea3bb0de6
Ross Leitch <ross@hexhive.io>
2022-10-17 19:46:28 +1300

    Valve

--- dist/Water.js
+++ dist/Water.js
@@ -25,18 +25,14 @@ var __importStar = (this && this.__importStar) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.Water = void 0;
 const React = __importStar(require("react"));
-const Water = (props) => {
-    console.log({ props });
-    return (React.createElement("svg", Object.assign({ xmlns: "http://www.w3.org/2000/svg", viewBox: "0 0 100 100", xmlSpace: "preserve", role: "img" }, props),
-        React.createElement("path", { opacity: 0.5, fill: "#4AE5FF", d: "M28.81 23.22h37.33v37.33H28.81z" }),
-        React.createElement("path", { fill: "none", stroke: "#4AE5FF", strokeMiterlimit: 10, d: "M28.81 23.22h37.33v37.33H28.81z" })));
-};
+const Water = (props) => (React.createElement("svg", Object.assign({ xmlns: "http://www.w3.org/2000/svg", viewBox: "0 0 40.1 40.1", xmlSpace: "preserve", role: "img" }, props),
+    React.createElement("path", { opacity: 0.5, fill: "#4AE5FF", d: "M1.4 1.4h37.3v37.3H1.4z" }),
+    React.createElement("path", { fill: "none", stroke: "#4AE5FF", strokeMiterlimit: 10, d: "M1.4 1.4h37.3v37.3H1.4z" })));
 exports.Water = Water;
 exports.Water.metadata = {
-    width: 50,
-    height: 50,
+    width: 100,
+    height: 100,
     options: {
-        color: 'String',
         level: 'Number'
     }
 };
--- src/Water.tsx
+++ src/Water.tsx
@@ -1,31 +1,28 @@
 import * as React from "react";
 import { SVGProps } from "react";
-
-export const Water = (props: SVGProps<SVGSVGElement>) => {
-  console.log({props});
-  
-  return (<svg
+export const Water = (props: SVGProps<SVGSVGElement>) => (
+  <svg
     xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 100 100"
+    viewBox="0 0 40.1 40.1"
     xmlSpace="preserve"
     role="img"
     {...props}
   >
-    <path opacity={0.5} fill="#4AE5FF" d="M28.81 23.22h37.33v37.33H28.81z" />
+    <path opacity={0.5} fill="#4AE5FF" d="M1.4 1.4h37.3v37.3H1.4z" />
     <path
       fill="none"
       stroke="#4AE5FF"
       strokeMiterlimit={10}
-      d="M28.81 23.22h37.33v37.33H28.81z"
+      d="M1.4 1.4h37.3v37.3H1.4z"
     />
-  </svg>)
-};
+  </svg>
+);
 
 Water.metadata = {
-  width: 50,
-  height: 50,
+  width: 100,
+  height: 100,
   options: {
-    color: 'String',
     level: 'Number'
   }
 }
+
