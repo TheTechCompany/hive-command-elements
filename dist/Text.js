2d073c3a8d551cc5fefa0069e6d66e1b81d0501f
Ross Leitch <ross@hexhive.io>
2022-10-26 13:17:08 +1300

    Build ports

--- dist/Text.js
+++ dist/Text.js
@@ -7,7 +7,11 @@ exports.Text = void 0;
 const material_1 = require("@mui/material");
 const react_1 = __importDefault(require("react"));
 const Text = (props) => {
-    return (react_1.default.createElement(material_1.Typography, null, props.children || "Text"));
+    return (react_1.default.createElement(material_1.Box, { sx: {
+            border: props.border ? '1px solid black' : undefined,
+            borerRadius: props.borderRadius != undefined ? `${props.borderRadius}px` : undefined
+        } },
+        react_1.default.createElement(material_1.Typography, null, props.text || props.children || "Text")));
 };
 exports.Text = Text;
 exports.Text.metadata = {
--- src/Text.tsx
+++ src/Text.tsx
@@ -1,9 +1,14 @@
-import { Typography } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 import React from "react";
 
 export const Text = (props: any) => {
     return (
-        <Typography>{props.children || "Text"}</Typography>
+        <Box sx={{
+            border: props.border ? '1px solid black' : undefined,
+            borerRadius: props.borderRadius != undefined ? `${props.borderRadius}px` : undefined
+        }}>
+            <Typography>{props.text || props.children || "Text"}</Typography>
+        </Box>
     )
 }
 
