723eb6137e0e58f3fe6befd7b0f071f657d5190f
Ross Leitch <ross@hexhive.io>
2022-11-09 15:47:38 +1300

    Added automatic horizon to timeline

--- dist/TJoin.js
+++ dist/TJoin.js
@@ -25,8 +25,8 @@ var __importStar = (this && this.__importStar) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.TJoin = void 0;
 const React = __importStar(require("react"));
-const TJoin = (props) => (React.createElement("svg", Object.assign({ xmlns: "http://www.w3.org/2000/svg", viewBox: "0 0 160.8 100", xmlSpace: "preserve" }, props),
-    React.createElement("linearGradient", { id: "TJoin_svg__a", gradientUnits: "userSpaceOnUse", x1: 70.24, y1: 54.78, x2: 90.236, y2: 54.78 },
+const TJoin = (props) => (React.createElement("svg", Object.assign({ xmlns: "http://www.w3.org/2000/svg", viewBox: "0 0 93.9 58.4", xmlSpace: "preserve" }, props),
+    React.createElement("linearGradient", { id: "TJOIN_svg__a", gradientUnits: "userSpaceOnUse", x1: 36.798, y1: 26.203, x2: 56.898, y2: 26.203, gradientTransform: "matrix(1 0 0 -1 0 60.406)" },
         React.createElement("stop", { offset: 0, style: {
                 stopColor: "#6f7173",
             } }),
@@ -36,8 +36,10 @@ const TJoin = (props) => (React.createElement("svg", Object.assign({ xmlns: "htt
         React.createElement("stop", { offset: 1, style: {
                 stopColor: "#6f7173",
             } })),
-    React.createElement("path", { fill: "url(#TJoin_svg__a)", d: "M70.2 89.8h20v-60l-10-10-10 10z" }),
-    React.createElement("linearGradient", { id: "TJoin_svg__b", gradientUnits: "userSpaceOnUse", x1: 80.238, y1: 29.785, x2: 80.238, y2: 9.788 },
+    React.createElement("path", { style: {
+            fill: "url(#TJOIN_svg__a)",
+        }, d: "M36.8 51.7h20l.1-25-10-10-10 10z" }),
+    React.createElement("linearGradient", { id: "TJOIN_svg__b", gradientUnits: "userSpaceOnUse", x1: 46.104, y1: 33.703, x2: 46.104, y2: 53.703, gradientTransform: "matrix(1 0 0 -1 0 60.406)" },
         React.createElement("stop", { offset: 0, style: {
                 stopColor: "#6f7173",
             } }),
@@ -47,40 +49,9 @@ const TJoin = (props) => (React.createElement("svg", Object.assign({ xmlns: "htt
         React.createElement("stop", { offset: 1, style: {
                 stopColor: "#6f7173",
             } })),
-    React.createElement("path", { fill: "url(#TJoin_svg__b)", d: "M10.3 29.8h60l10-10 10 10h60v-20h-140v20z" }),
-    React.createElement("linearGradient", { id: "TJoin_svg__c", gradientUnits: "userSpaceOnUse", x1: -313.724, y1: 650.895, x2: -313.724, y2: 627.577, gradientTransform: "rotate(180 -83.549 329.511)" },
-        React.createElement("stop", { offset: 0, style: {
-                stopColor: "#333334",
-            } }),
-        React.createElement("stop", { offset: 0.501, style: {
-                stopColor: "#7d7e81",
-            } }),
-        React.createElement("stop", { offset: 1, style: {
-                stopColor: "#333334",
-            } })),
-    React.createElement("path", { fill: "url(#TJoin_svg__c)", d: "M143 8.1h7.2v23.3H143z" }),
-    React.createElement("linearGradient", { id: "TJoin_svg__d", gradientUnits: "userSpaceOnUse", x1: -180.946, y1: 650.895, x2: -180.946, y2: 627.577, gradientTransform: "rotate(180 -83.549 329.511)" },
-        React.createElement("stop", { offset: 0, style: {
-                stopColor: "#333334",
-            } }),
-        React.createElement("stop", { offset: 0.501, style: {
-                stopColor: "#7d7e81",
-            } }),
-        React.createElement("stop", { offset: 1, style: {
-                stopColor: "#333334",
-            } })),
-    React.createElement("path", { fill: "url(#TJoin_svg__d)", d: "M10.3 8.1h7.1v23.3h-7.1z" }),
-    React.createElement("linearGradient", { id: "TJoin_svg__e", gradientUnits: "userSpaceOnUse", x1: 232.933, y1: 628.092, x2: 232.933, y2: 604.775, gradientTransform: "rotate(-90 -108.544 427.652)" },
-        React.createElement("stop", { offset: 0, style: {
-                stopColor: "#333334",
-            } }),
-        React.createElement("stop", { offset: 0.501, style: {
-                stopColor: "#7d7e81",
-            } }),
-        React.createElement("stop", { offset: 1, style: {
-                stopColor: "#333334",
-            } })),
-    React.createElement("path", { fill: "url(#TJoin_svg__e)", d: "M91.9 82.6v7.2H68.6v-7.2z" })));
+    React.createElement("path", { d: "M15.2 26.7h21.7l10-10 10 10H77v-20H15.2v20z", style: {
+            fill: "url(#TJOIN_svg__b)",
+        } })));
 exports.TJoin = TJoin;
 exports.TJoin.metadata = {
     width: 100,
--- src/TJoin.tsx
+++ src/TJoin.tsx
@@ -4,45 +4,18 @@ import { SVGProps } from "react";
 export const TJoin = (props: SVGProps<SVGSVGElement>) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 160.8 100"
+    viewBox="0 0 93.9 58.4"
     xmlSpace="preserve"
     {...props}
   >
     <linearGradient
-      id="TJoin_svg__a"
+      id="TJOIN_svg__a"
       gradientUnits="userSpaceOnUse"
-      x1={70.24}
-      y1={54.78}
-      x2={90.236}
-      y2={54.78}
-    >
-      <stop
-        offset={0}
-        style={{
-          stopColor: "#6f7173",
-        }}
-      />
-      <stop
-        offset={0.501}
-        style={{
-          stopColor: "#d3d5d6",
-        }}
-      />
-      <stop
-        offset={1}
-        style={{
-          stopColor: "#6f7173",
-        }}
-      />
-    </linearGradient>
-    <path fill="url(#TJoin_svg__a)" d="M70.2 89.8h20v-60l-10-10-10 10z" />
-    <linearGradient
-      id="TJoin_svg__b"
-      gradientUnits="userSpaceOnUse"
-      x1={80.238}
-      y1={29.785}
-      x2={80.238}
-      y2={9.788}
+      x1={36.798}
+      y1={26.203}
+      x2={56.898}
+      y2={26.203}
+      gradientTransform="matrix(1 0 0 -1 0 60.406)"
     >
       <stop
         offset={0}
@@ -64,98 +37,48 @@ export const TJoin = (props: SVGProps<SVGSVGElement>) => (
       />
     </linearGradient>
     <path
-      fill="url(#TJoin_svg__b)"
-      d="M10.3 29.8h60l10-10 10 10h60v-20h-140v20z"
+      style={{
+        fill: "url(#TJOIN_svg__a)",
+      }}
+      d="M36.8 51.7h20l.1-25-10-10-10 10z"
     />
     <linearGradient
-      id="TJoin_svg__c"
+      id="TJOIN_svg__b"
       gradientUnits="userSpaceOnUse"
-      x1={-313.724}
-      y1={650.895}
-      x2={-313.724}
-      y2={627.577}
-      gradientTransform="rotate(180 -83.549 329.511)"
+      x1={46.104}
+      y1={33.703}
+      x2={46.104}
+      y2={53.703}
+      gradientTransform="matrix(1 0 0 -1 0 60.406)"
     >
       <stop
         offset={0}
         style={{
-          stopColor: "#333334",
+          stopColor: "#6f7173",
         }}
       />
       <stop
         offset={0.501}
         style={{
-          stopColor: "#7d7e81",
+          stopColor: "#d3d5d6",
         }}
       />
       <stop
         offset={1}
         style={{
-          stopColor: "#333334",
+          stopColor: "#6f7173",
         }}
       />
     </linearGradient>
-    <path fill="url(#TJoin_svg__c)" d="M143 8.1h7.2v23.3H143z" />
-    <linearGradient
-      id="TJoin_svg__d"
-      gradientUnits="userSpaceOnUse"
-      x1={-180.946}
-      y1={650.895}
-      x2={-180.946}
-      y2={627.577}
-      gradientTransform="rotate(180 -83.549 329.511)"
-    >
-      <stop
-        offset={0}
-        style={{
-          stopColor: "#333334",
-        }}
-      />
-      <stop
-        offset={0.501}
-        style={{
-          stopColor: "#7d7e81",
-        }}
-      />
-      <stop
-        offset={1}
-        style={{
-          stopColor: "#333334",
-        }}
-      />
-    </linearGradient>
-    <path fill="url(#TJoin_svg__d)" d="M10.3 8.1h7.1v23.3h-7.1z" />
-    <linearGradient
-      id="TJoin_svg__e"
-      gradientUnits="userSpaceOnUse"
-      x1={232.933}
-      y1={628.092}
-      x2={232.933}
-      y2={604.775}
-      gradientTransform="rotate(-90 -108.544 427.652)"
-    >
-      <stop
-        offset={0}
-        style={{
-          stopColor: "#333334",
-        }}
-      />
-      <stop
-        offset={0.501}
-        style={{
-          stopColor: "#7d7e81",
-        }}
-      />
-      <stop
-        offset={1}
-        style={{
-          stopColor: "#333334",
-        }}
-      />
-    </linearGradient>
-    <path fill="url(#TJoin_svg__e)" d="M91.9 82.6v7.2H68.6v-7.2z" />
+    <path
+      d="M15.2 26.7h21.7l10-10 10 10H77v-20H15.2v20z"
+      style={{
+        fill: "url(#TJOIN_svg__b)",
+      }}
+    />
   </svg>
 );
+
 
 TJoin.metadata = {
   width: 100,
@@ -182,3 +105,4 @@ TJoin.metadata = {
     }
   ]
 }
+
