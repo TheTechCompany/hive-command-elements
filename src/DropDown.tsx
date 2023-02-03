600b275392d0070e4d413ef872e2d9958fe3bbd6
Ross Leitch <ross@hexhive.io>
2023-02-03 20:52:20 +1300

    Dropdown

---
+++ src/DropDown.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Select, FormControl, InputLabel, MenuItem  } from '@mui/material'
+
+export interface DropdownProps {
+    options?: {
+        label: string,
+        options: string[],
+        value: string,
+        onChange: (value: string) => void;
+    }
+}
+
+export const Dropdown : React.FC<DropdownProps> = (props) => {
+    return (
+        <FormControl>
+            <InputLabel>{props.options?.label}</InputLabel>
+            <Select
+                label={props.options?.label}
+                value={props.options?.value}
+                onChange={props.options?.onChange && ((e) => props.options?.onChange(e.target.value as string))}>
+                    {props.options?.options?.map((option) => (
+                        <MenuItem value={option}>{option}</MenuItem>
+                    ))}
+            </Select>
+        </FormControl>
+    )
+}
+
+(Dropdown as any).metadata = {
+    width: 125,
+    height: 50,
+    options: {
+        label: 'String',
+        options: '[String]',
+        value: 'String',
+        onChange: 'Function'
+    }
+}
