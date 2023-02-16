0678d9fcecb3c29c7f0069569a2b4239cf71928b
Ross Leitch <ross@hexhive.io>
2023-02-16 15:41:58 +1300

    TextFIeld

--- src/Water.tsx
+++ src/Water.tsx
@@ -1,34 +1,38 @@
 import * as React from "react";
 import { SVGProps } from "react";
 
-export const Water = (props: any) => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    viewBox="2.5 2 40 40"
-    xmlSpace="preserve"
-    preserveAspectRatio="none"
-    style={{
-      width: '100%',
-      height: '100%',
-        transformOrigin: '0 bottom',
-        transform: `scaleY(${(parseFloat(props.options?.level || 100) / 100) || 1})`
-    }}
-    {...props}
-  >
-    <path
-      fill="#4AE5FF"
-      d="M41.4 35.5c0 2.8-2.2 5-5 5H9c-2.8 0-5-2.2-5-5V8.1c0-2.8 2.2-5 5-5h27.3c2.8 0 5 2.3 5 5v27.4z"
-      opacity={0.5}
-    />
-    <path
-      fill="none"
-      stroke="#4AE5FF"
-      strokeMiterlimit={10}
-      d="M41.4 35.5c0 2.8-2.2 5-5 5H9c-2.8 0-5-2.2-5-5V8.1c0-2.8 2.2-5 5-5h27.3c2.8 0 5 2.3 5 5v27.4z"
-    />
-  </svg>
-);
+export const Water = (props: {options?: { level: number} }) => {
+console.log("Water level", props.options?.level);
 
+  return (
+    
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="2.5 2 40 40"
+      xmlSpace="preserve"
+      preserveAspectRatio="none"
+      style={{
+        width: '100%',
+        height: '100%',
+          transformOrigin: '0 bottom',
+          transform: `scaleY(${((props.options?.level || 100) / 100) || 1})`
+      }}
+      {...props}
+    >
+      <path
+        fill="#4AE5FF"
+        d="M41.4 35.5c0 2.8-2.2 5-5 5H9c-2.8 0-5-2.2-5-5V8.1c0-2.8 2.2-5 5-5h27.3c2.8 0 5 2.3 5 5v27.4z"
+        opacity={0.5}
+      />
+      <path
+        fill="none"
+        stroke="#4AE5FF"
+        strokeMiterlimit={10}
+        d="M41.4 35.5c0 2.8-2.2 5-5 5H9c-2.8 0-5-2.2-5-5V8.1c0-2.8 2.2-5 5-5h27.3c2.8 0 5 2.3 5 5v27.4z"
+      />
+    </svg>
+  );
+}
 Water.metadata = {
     width: 100,
     height: 100,
