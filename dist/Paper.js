962f47be1b4d402eb736f5148b5ec08b1006d6ee
Ross Leitch <ross@hexhive.io>
2023-02-20 13:09:06 +1300

    Paper

---
+++ dist/Paper.js
@@ -0,0 +1,16 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Paper = void 0;
+const react_1 = __importDefault(require("react"));
+const material_1 = require("@mui/material");
+const Paper = () => {
+    return (react_1.default.createElement(material_1.Paper, { sx: { flex: 1 } }));
+};
+exports.Paper = Paper;
+exports.Paper.metadata = {
+    width: 100,
+    height: 100
+};
---
+++ src/Paper.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Paper as MuiPaper } from '@mui/material'
+
+export const Paper = () => {
+    return (
+        <MuiPaper sx={{flex: 1}} />
+    )
+}
+
+Paper.metadata = {
+    width: 100,
+    height: 100
+};
