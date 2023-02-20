e09c4d39a126b50da4486e61af4365620b73a6d0
Ross Leitch <ross@hexhive.io>
2023-02-20 16:26:09 +1300

    TJoin

--- dist/PrimRectangle.js
+++ dist/PrimRectangle.js
@@ -7,7 +7,7 @@ exports.Rectangle = void 0;
 const react_1 = __importDefault(require("react"));
 const Rectangle = (props) => {
     var _a, _b, _c, _d, _e, _f, _g;
-    const strokeWidth = ((_a = props.options) === null || _a === void 0 ? void 0 : _a.strokeWidth) || 0;
+    const strokeWidth = parseFloat(((_a = props.options) === null || _a === void 0 ? void 0 : _a.strokeWidth) || 0);
     return (react_1.default.createElement("svg", { width: props.width, height: props.height },
         react_1.default.createElement("rect", { x: strokeWidth / 2, y: strokeWidth / 2, fill: ((_b = props.options) === null || _b === void 0 ? void 0 : _b.fill) || 'none', width: (props === null || props === void 0 ? void 0 : props.width) - (strokeWidth + (strokeWidth / 2)), height: (props === null || props === void 0 ? void 0 : props.height) - (strokeWidth + (strokeWidth / 2)), strokeWidth: (_c = props.options) === null || _c === void 0 ? void 0 : _c.strokeWidth, stroke: ((_d = props.options) === null || _d === void 0 ? void 0 : _d.strokeColor) || (!((_e = props.options) === null || _e === void 0 ? void 0 : _e.fill) ? 'black' : undefined), rx: (_f = props.options) === null || _f === void 0 ? void 0 : _f.rounded, ry: (_g = props.options) === null || _g === void 0 ? void 0 : _g.rounded })));
 };
--- src/PrimRectangle.tsx
+++ src/PrimRectangle.tsx
@@ -2,7 +2,8 @@ import React from 'react';
 
 export const Rectangle = (props: {options: any, width: number, height: number}) => {
 
-    const strokeWidth = props.options?.strokeWidth || 0;
+    const strokeWidth = parseFloat(props.options?.strokeWidth || 0);
+
 
     return (
         <svg width={props.width} height={props.height}>
@@ -10,7 +11,7 @@ export const Rectangle = (props: {options: any, width: number, height: number}) 
                 x={strokeWidth / 2}
                 y={strokeWidth / 2}
                 fill={props.options?.fill || 'none'}
-                width={props?.width - (strokeWidth+ (strokeWidth / 2))}
+                width={props?.width - (strokeWidth + (strokeWidth / 2))}
                 height={props?.height - (strokeWidth + (strokeWidth / 2))}
                 strokeWidth={props.options?.strokeWidth}
                 stroke={props.options?.strokeColor || (!props.options?.fill ? 'black' : undefined)}
