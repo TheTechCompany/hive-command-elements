bc9c0995d38c4264bfd4746e3ab7675f8b49f654
Ross Leitch <ross@hexhive.io>
2023-02-20 13:03:58 +1300

    Replaced

--- dist/Text.js
+++ dist/Text.js
@@ -7,18 +7,22 @@ exports.Text = void 0;
 const material_1 = require("@mui/material");
 const react_1 = __importDefault(require("react"));
 const Text = (props) => {
-    var _a, _b, _c, _d;
+    var _a, _b, _c, _d, _e;
     return (react_1.default.createElement(material_1.Box, { sx: {
-            border: ((_a = props.options) === null || _a === void 0 ? void 0 : _a.border) ? '1px solid black' : undefined,
-            borerRadius: ((_b = props.options) === null || _b === void 0 ? void 0 : _b.borderRadius) != undefined ? `${(_c = props.options) === null || _c === void 0 ? void 0 : _c.borderRadius}px` : undefined
+            background: ((_a = props.options) === null || _a === void 0 ? void 0 : _a.background) || 'white',
+            paddingLeft: '6px',
+            paddingRight: '6px',
+            border: ((_b = props.options) === null || _b === void 0 ? void 0 : _b.border) ? '1px solid black' : undefined,
+            borerRadius: ((_c = props.options) === null || _c === void 0 ? void 0 : _c.borderRadius) != undefined ? `${(_d = props.options) === null || _d === void 0 ? void 0 : _d.borderRadius}px` : undefined
         } },
-        react_1.default.createElement(material_1.Typography, { fontSize: 'small' }, ((_d = props.options) === null || _d === void 0 ? void 0 : _d.text) || props.children || "Text")));
+        react_1.default.createElement(material_1.Typography, { fontSize: 'small' }, ((_e = props.options) === null || _e === void 0 ? void 0 : _e.text) || props.children || "Text")));
 };
 exports.Text = Text;
 exports.Text.metadata = {
     width: 80,
     height: 20,
     options: {
+        background: 'String',
         text: 'String',
         border: 'Boolean',
         borderRadius: 'Number'
--- src/Text.tsx
+++ src/Text.tsx
@@ -1,9 +1,12 @@
 import { Box, Typography } from "@mui/material";
 import React from "react";
 
-export const Text = (props: {options: {border: boolean, borderRadius: number, text: string}, children: any}) => {
+export const Text = (props: {options: {background: string, border: boolean, borderRadius: number, text: string}, children: any}) => {
     return (
         <Box sx={{
+            background: props.options?.background || 'white',
+            paddingLeft: '6px',
+            paddingRight: '6px',
             border: props.options?.border ? '1px solid black' : undefined,
             borerRadius: props.options?.borderRadius != undefined ? `${props.options?.borderRadius}px` : undefined
         }}>
@@ -16,6 +19,7 @@ Text.metadata = {
     width: 80,
     height: 20,    
     options: {
+        background: 'String',
         text: 'String',
         border: 'Boolean',
         borderRadius: 'Number'
