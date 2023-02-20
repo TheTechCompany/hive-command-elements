a9524e37cb5a68a05087fc6490473f02d4a4a9d6
Ross Leitch <ross@hexhive.io>
2023-02-20 15:42:58 +1300

    Fan

--- src/PrimRectangle.tsx
+++ src/PrimRectangle.tsx
@@ -5,6 +5,8 @@ export const Rectangle = (props: {options: any, width: number, height: number}) 
     return (
         <svg width={props.width + ((props.options?.strokeWidth || 0) * 2) } height={props.height + ((props.options?.strokeWidth || 0) * 2) }>
             <rect  
+                x={props.options?.strokeWidth}
+                y={props.options?.strokeWidth}
                 fill={props.options?.fill || 'none'}
                 width={props?.width}
                 height={props?.height}
