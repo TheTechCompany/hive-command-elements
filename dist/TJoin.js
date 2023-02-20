b50b97c3a28ed8593ee021cea05b7bc90ad4d0b6
Ross Leitch <ross@hexhive.io>
2023-02-20 15:26:46 +1300

    Fan

--- dist/TJoin.js
+++ dist/TJoin.js
@@ -25,33 +25,10 @@ var __importStar = (this && this.__importStar) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.TJoin = void 0;
 const React = __importStar(require("react"));
-const TJoin = (props) => (React.createElement("svg", Object.assign({ xmlns: "http://www.w3.org/2000/svg", viewBox: "0 0 93.9 58.4", xmlSpace: "preserve" }, props),
-    React.createElement("linearGradient", { id: "TJOIN_svg__a", gradientUnits: "userSpaceOnUse", x1: 36.798, y1: 26.203, x2: 56.898, y2: 26.203, gradientTransform: "matrix(1 0 0 -1 0 60.406)" },
-        React.createElement("stop", { offset: 0, style: {
-                stopColor: "#6f7173",
-            } }),
-        React.createElement("stop", { offset: 0.501, style: {
-                stopColor: "#d3d5d6",
-            } }),
-        React.createElement("stop", { offset: 1, style: {
-                stopColor: "#6f7173",
-            } })),
+const TJoin = (props) => (React.createElement("svg", Object.assign({ xmlns: "http://www.w3.org/2000/svg", viewBox: "0 0 283.5 283.5", xmlSpace: "preserve" }, props),
     React.createElement("path", { style: {
-            fill: "url(#TJOIN_svg__a)",
-        }, d: "M36.8 51.7h20l.1-25-10-10-10 10z" }),
-    React.createElement("linearGradient", { id: "TJOIN_svg__b", gradientUnits: "userSpaceOnUse", x1: 46.104, y1: 33.703, x2: 46.104, y2: 53.703, gradientTransform: "matrix(1 0 0 -1 0 60.406)" },
-        React.createElement("stop", { offset: 0, style: {
-                stopColor: "#6f7173",
-            } }),
-        React.createElement("stop", { offset: 0.501, style: {
-                stopColor: "#d3d5d6",
-            } }),
-        React.createElement("stop", { offset: 1, style: {
-                stopColor: "#6f7173",
-            } })),
-    React.createElement("path", { d: "M15.2 26.7h21.7l10-10 10 10H77v-20H15.2v20z", style: {
-            fill: "url(#TJOIN_svg__b)",
-        } })));
+            fill: "#939393",
+        }, d: "M189 0H0v94.5h94.5V189H189V94.5h94.5V0z" })));
 exports.TJoin = TJoin;
 exports.TJoin.metadata = {
     width: 60,
