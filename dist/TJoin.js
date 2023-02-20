1b06f35680c7ee31413161d5bd907e397a3d8c1d
Ross Leitch <ross@hexhive.io>
2023-02-20 16:11:45 +1300

    TJoin

--- dist/TJoin.js
+++ dist/TJoin.js
@@ -25,10 +25,12 @@ var __importStar = (this && this.__importStar) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.TJoin = void 0;
 const React = __importStar(require("react"));
-const TJoin = (props) => (React.createElement("svg", Object.assign({ xmlns: "http://www.w3.org/2000/svg", viewBox: "0 0 283.5 283.5", xmlSpace: "preserve" }, props),
+const TJoin = (props) => (React.createElement("svg", Object.assign({ id: "TJoin_svg__Layer_1", xmlns: "http://www.w3.org/2000/svg", x: 0, y: 0, viewBox: "0 0 283.5 283.5", xmlSpace: "preserve" }, props),
+    React.createElement("style", null, ".TJoin_svg__st0{opacity:.8;fill:none;stroke:#626363;stroke-width:3.5902;stroke-miterlimit:10}"),
+    React.createElement("path", { className: "TJoin_svg__st0", d: "M277.5 56.9H5.7M170 221.2V113.5h107.5M5.7 113.5h107.8v107.7" }),
     React.createElement("path", { style: {
             fill: "#939393",
-        }, d: "M189 0H0v94.5h94.5V189H189V94.5h94.5V0z" })));
+        }, d: "M6 58v54.3h108.6v108.6h54.3V112.3h108.6V58z" })));
 exports.TJoin = TJoin;
 exports.TJoin.metadata = {
     width: 60,
--- src/TJoin.tsx
+++ src/TJoin.tsx
@@ -3,16 +3,29 @@ import { SVGProps } from "react";
 
 export const TJoin = (props: SVGProps<SVGSVGElement>) => (
   <svg
+    id="TJoin_svg__Layer_1"
     xmlns="http://www.w3.org/2000/svg"
+    x={0}
+    y={0}
     viewBox="0 0 283.5 283.5"
+
     xmlSpace="preserve"
     {...props}
   >
+    <style>
+      {
+        ".TJoin_svg__st0{opacity:.8;fill:none;stroke:#626363;stroke-width:3.5902;stroke-miterlimit:10}"
+      }
+    </style>
+    <path
+      className="TJoin_svg__st0"
+      d="M277.5 56.9H5.7M170 221.2V113.5h107.5M5.7 113.5h107.8v107.7"
+    />
     <path
       style={{
         fill: "#939393",
       }}
-      d="M189 0H0v94.5h94.5V189H189V94.5h94.5V0z"
+      d="M6 58v54.3h108.6v108.6h54.3V112.3h108.6V58z"
     />
   </svg>
 );
