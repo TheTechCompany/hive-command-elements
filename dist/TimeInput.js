e01482a03dcdfdca08ddd2b4f100233c59d25f71
Ross Leitch <ross@hexhive.io>
2023-02-23 14:48:31 +1300

    Mobile build

--- dist/TimeInput.js
+++ dist/TimeInput.js
@@ -10,7 +10,7 @@ const react_1 = __importDefault(require("react"));
 const TimeInput = (props) => {
     var _a, _b;
     return (react_1.default.createElement(material_1.Box, { sx: { flex: 1, display: 'flex' } },
-        react_1.default.createElement(x_date_pickers_1.TimePicker, { label: (_a = props.options) === null || _a === void 0 ? void 0 : _a.label, onChange: (value) => { var _a, _b; return value && ((_b = (_a = props.options) === null || _a === void 0 ? void 0 : _a.onChange) === null || _b === void 0 ? void 0 : _b.call(_a, value)); }, value: (_b = props.options) === null || _b === void 0 ? void 0 : _b.value, renderInput: (params) => react_1.default.createElement(material_1.TextField, Object.assign({ fullWidth: true, size: "small" }, params)) })));
+        react_1.default.createElement(x_date_pickers_1.TimePicker, { label: (_a = props.options) === null || _a === void 0 ? void 0 : _a.label, inputFormat: `hh:mma`, onChange: (value) => { var _a, _b; return value && ((_b = (_a = props.options) === null || _a === void 0 ? void 0 : _a.onChange) === null || _b === void 0 ? void 0 : _b.call(_a, value.toDate())); }, value: (_b = props.options) === null || _b === void 0 ? void 0 : _b.value, renderInput: (params) => react_1.default.createElement(material_1.TextField, Object.assign({ fullWidth: true, size: "small" }, params)) })));
 };
 exports.TimeInput = TimeInput;
 exports.TimeInput.metadata = {
