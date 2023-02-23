7282db11599d9be313c3939649c8b4df00e66f4b
Ross Leitch <ross@hexhive.io>
2023-02-23 14:54:39 +1300

    Mobile build

--- dist/DateInput.js
+++ dist/DateInput.js
@@ -10,7 +10,7 @@ const react_1 = __importDefault(require("react"));
 const DateInput = (props) => {
     var _a, _b;
     return (react_1.default.createElement(material_1.Box, { sx: { flex: 1, display: 'flex' } },
-        react_1.default.createElement(x_date_pickers_1.DatePicker, { label: (_a = props.options) === null || _a === void 0 ? void 0 : _a.label, inputFormat: `dd/mm`, onChange: (value) => { var _a, _b; return value && ((_b = (_a = props.options) === null || _a === void 0 ? void 0 : _a.onChange) === null || _b === void 0 ? void 0 : _b.call(_a, value.toDate())); }, value: (_b = props.options) === null || _b === void 0 ? void 0 : _b.value, renderInput: (params) => react_1.default.createElement(material_1.TextField, Object.assign({ fullWidth: true, size: "small" }, params)) })));
+        react_1.default.createElement(x_date_pickers_1.DatePicker, { label: (_a = props.options) === null || _a === void 0 ? void 0 : _a.label, inputFormat: `DD/MM`, onChange: (value) => { var _a, _b; return value && ((_b = (_a = props.options) === null || _a === void 0 ? void 0 : _a.onChange) === null || _b === void 0 ? void 0 : _b.call(_a, value.toDate())); }, value: (_b = props.options) === null || _b === void 0 ? void 0 : _b.value, renderInput: (params) => react_1.default.createElement(material_1.TextField, Object.assign({ fullWidth: true, size: "small" }, params)) })));
 };
 exports.DateInput = DateInput;
 exports.DateInput.metadata = {
