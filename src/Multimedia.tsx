8ad08562fb0745c36a1f97e4c5ac15cac55e57dc
Ross Leitch <ross@hexhive.io>
2023-12-21 08:16:00 +1300

    UV

---
+++ src/Multimedia.tsx
@@ -0,0 +1,43 @@
+import * as React from "react";
+import type { SVGProps } from "react";
+
+export const Multimedia = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    xmlSpace="preserve"
+    id="multimedia_svg__Layer_1"
+    x={0}
+    y={0}
+    viewBox="0 0 31.5 97.4"
+    {...props}
+  >
+    <style>{".multimedia_svg__st0{fill:#4d4d4e}"}</style>
+    <path
+      d="M10 19.6c1.3 1 3.3 1.5 5.7 1.5 2.4 0 4.4-.6 5.7-1.5 3.8 2.1 6.3 6.1 6.3 10.6v62.5c0 .6-.5 1.1-1.1 1.1H4.8c-.6 0-1.1-.5-1.1-1.1V30.1c0-4.4 2.5-8.5 6.3-10.5m10.4-3.7c-.2 1.5-2.4 2.2-4.7 2.2-2.3 0-4.5-.7-4.7-2.2C5 17.9.7 23.5.7 30.1v62.5c0 2.3 1.8 4.1 4.1 4.1h21.8c2.3 0 4.1-1.8 4.1-4.1V30.1c0-6.6-4.3-12.2-10.3-14.2z"
+      className="multimedia_svg__st0"
+    />
+    <path
+      d="M27.7 3.6v2.2h-8.3c-1.7 0-3 1.3-3 3v4.3H15V8.8c0-1.7-1.3-3-3-3H3.7V3.6h24m.6-3H3.1C1.8.6.7 1.7.7 3v3.4c0 1.3 1.1 2.4 2.4 2.4H12v5.9c0 .8 1.7 1.4 3.7 1.4s3.7-.6 3.7-1.4V8.8h8.9c1.3 0 2.4-1.1 2.4-2.4V3c0-1.3-1.1-2.4-2.4-2.4z"
+      className="multimedia_svg__st0"
+    />
+  </svg>
+);
+
+Multimedia.metadata = {
+  type: 'multimedia',
+  ports: [
+    {
+      key: 'outlet',
+      momentum: 1,
+      x: '80%',
+      y: '42%',
+      rotation: 180
+    },
+    {
+      key: 'inlet',
+      momentum: -1,
+      x: '-5%',
+      y: '42%'
+    }
+  ]
+}
---
+++ src/UV.tsx
@@ -0,0 +1,137 @@
+import * as React from "react";
+import type { SVGProps } from "react";
+
+export const UV = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    xmlSpace="preserve"
+    id="uv_svg__Layer_1"
+    x={0}
+    y={0}
+    viewBox="0 0 165.4 45.3"
+    {...props}
+  >
+    <style>{".uv_svg__st1{fill:#4d4d4e}"}</style>
+    <linearGradient
+      id="uv_svg__SVGID_1_"
+      x1={75.411}
+      x2={75.411}
+      y1={14.88}
+      y2={44.88}
+      gradientUnits="userSpaceOnUse"
+    >
+      <stop
+        offset={0}
+        style={{
+          stopColor: "#393839",
+        }}
+      />
+      <stop
+        offset={0.5}
+        style={{
+          stopColor: "#7e7f81",
+        }}
+      />
+      <stop
+        offset={1}
+        style={{
+          stopColor: "#393839",
+        }}
+      />
+    </linearGradient>
+    <path
+      d="M146.9 44.9H3.9C2 44.9.4 43.3.4 41.4v-23c0-1.9 1.6-3.5 3.5-3.5h143c1.9 0 3.5 1.6 3.5 3.5v23c0 1.9-1.6 3.5-3.5 3.5z"
+      style={{
+        fill: "url(#uv_svg__SVGID_1_)",
+      }}
+    />
+    <path d="M6.27 14.902v-10h17.6v10z" className="uv_svg__st1" />
+    <linearGradient
+      id="uv_svg__SVGID_00000114042839260797354350000001853898132272339639_"
+      x1={0.411}
+      x2={30.411}
+      y1={2.614}
+      y2={2.614}
+      gradientUnits="userSpaceOnUse"
+    >
+      <stop
+        offset={0}
+        style={{
+          stopColor: "#393839",
+        }}
+      />
+      <stop
+        offset={0.5}
+        style={{
+          stopColor: "#7e7f81",
+        }}
+      />
+      <stop
+        offset={1}
+        style={{
+          stopColor: "#393839",
+        }}
+      />
+    </linearGradient>
+    <path
+      d="M30.4 2.6c0-1.2-1-2.2-2.2-2.2H2.6C1.4.4.4 1.4.4 2.6c0 1.2 1 2.2 2.2 2.2h25.5c1.3.1 2.3-.9 2.3-2.2z"
+      style={{
+        fill: "url(#uv_svg__SVGID_00000114042839260797354350000001853898132272339639_)",
+      }}
+    />
+    <path d="M150.4 20.7h10v17.6h-10z" className="uv_svg__st1" />
+    <linearGradient
+      id="uv_svg__SVGID_00000170243620128869902510000018192573999936894911_"
+      x1={162.677}
+      x2={162.677}
+      y1={14.88}
+      y2={44.88}
+      gradientUnits="userSpaceOnUse"
+    >
+      <stop
+        offset={0}
+        style={{
+          stopColor: "#393839",
+        }}
+      />
+      <stop
+        offset={0.5}
+        style={{
+          stopColor: "#7e7f81",
+        }}
+      />
+      <stop
+        offset={1}
+        style={{
+          stopColor: "#393839",
+        }}
+      />
+    </linearGradient>
+    <path
+      d="M162.7 44.9c1.2 0 2.2-1 2.2-2.2V17.1c0-1.2-1-2.2-2.2-2.2-1.2 0-2.2 1-2.2 2.2v25.5c-.1 1.3.9 2.3 2.2 2.3z"
+      style={{
+        fill: "url(#uv_svg__SVGID_00000170243620128869902510000018192573999936894911_)",
+      }}
+    />
+  </svg>
+);
+
+
+UV.metadata = {
+  type: 'uv',
+  ports: [
+    {
+      key: 'outlet',
+      momentum: 1,
+      x: '80%',
+      y: '42%',
+      rotation: 180
+    },
+    {
+      key: 'inlet',
+      momentum: -1,
+      x: '-5%',
+      y: '42%'
+    }
+  ]
+}
