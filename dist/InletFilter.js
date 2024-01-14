136c3ce01e6fd77de934140cd16131246d38a7f8
Ross Leitch <ross@hexhive.io>
2024-01-15 12:02:52 +1300

    Inlet filter

--- dist/InletFilter.js
+++ dist/InletFilter.js
@@ -26,11 +26,17 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.InletFilter = void 0;
 const React = __importStar(require("react"));
 const InletFilter = (props) => {
-    var _a, _b, _c;
-    return (React.createElement("svg", Object.assign({ xmlns: "http://www.w3.org/2000/svg", xmlSpace: "preserve", viewBox: "0 0 500 500", onClick: (_a = props.options) === null || _a === void 0 ? void 0 : _a.onClick }, props),
-        React.createElement("path", { d: "M494.8 55.2h-86.5V33.7h-83v64.7H175.9V33.7h-83v21.5H.3v62.1h56.2V175h39v215.4c0 41.9 34.1 76 76 76h158.3c41.9 0 76-34.1 76-76V175h39v-57.7h50.1V55.2zm-147.5.5h36.6v39h-36.6v-39zm-232.4 0h39v39h-39v-39zM27.1 82h57.3v8.5H27.1V82zm148.8 96.1h7.1v207.8h-7.1V178.1zM296 385.9h-6.3V178.1h6.3v207.8zm-91-207.8h7.8v207.8H205V178.1zm29.8 0h5.5v207.8h-5.5V178.1zm27.5 0h5.5v207.8h-5.5V178.1zm-43.7 229.8h63.9v3h-63.9v-3zM318 178.1h7.3v207.8H318V178.1zm65.7 212.3c0 29.8-24.2 54-54 54H171.5c-29.8 0-54-24.2-54-54V178.1h36.4v229.8h42.8v25h107.9v-25h42.8V178.1h36.4v212.3zm39-237.4H78.5v-32.6h344.2V153zM468 90.5h-57.3V82H468v8.5z", style: {
-                fill: ((_b = props.options) === null || _b === void 0 ? void 0 : _b.fill) || (!((_c = props.options) === null || _c === void 0 ? void 0 : _c.stroke)) ? "#4d4d4e" : 'none',
-            } })));
+    var _a, _b, _c, _d, _e;
+    console.log("Inlet filter opts", props);
+    return React.createElement("svg", Object.assign({ xmlns: "http://www.w3.org/2000/svg", xmlSpace: "preserve", viewBox: "0 0 500 500", onClick: (_a = props.options) === null || _a === void 0 ? void 0 : _a.onClick }, props),
+        React.createElement("style", null, `
+            .inlet_filter_svg{
+              fill:${((_b = props.options) === null || _b === void 0 ? void 0 : _b.fill) || (!((_c = props.options) === null || _c === void 0 ? void 0 : _c.stroke)) ? "#4d4d4e" : 'none'}
+            }
+        `),
+        React.createElement("path", { className: 'inlet_filter_svg', d: "M494.8 55.2h-86.5V33.7h-83v64.7H175.9V33.7h-83v21.5H.3v62.1h56.2V175h39v215.4c0 41.9 34.1 76 76 76h158.3c41.9 0 76-34.1 76-76V175h39v-57.7h50.1V55.2zm-147.5.5h36.6v39h-36.6v-39zm-232.4 0h39v39h-39v-39zM27.1 82h57.3v8.5H27.1V82zm148.8 96.1h7.1v207.8h-7.1V178.1zM296 385.9h-6.3V178.1h6.3v207.8zm-91-207.8h7.8v207.8H205V178.1zm29.8 0h5.5v207.8h-5.5V178.1zm27.5 0h5.5v207.8h-5.5V178.1zm-43.7 229.8h63.9v3h-63.9v-3zM318 178.1h7.3v207.8H318V178.1zm65.7 212.3c0 29.8-24.2 54-54 54H171.5c-29.8 0-54-24.2-54-54V178.1h36.4v229.8h42.8v25h107.9v-25h42.8V178.1h36.4v212.3zm39-237.4H78.5v-32.6h344.2V153zM468 90.5h-57.3V82H468v8.5z", style: {
+                fill: ((_d = props.options) === null || _d === void 0 ? void 0 : _d.fill) || (!((_e = props.options) === null || _e === void 0 ? void 0 : _e.stroke)) ? "#4d4d4e" : 'none',
+            } }));
 };
 exports.InletFilter = InletFilter;
 exports.InletFilter.metadata = {
