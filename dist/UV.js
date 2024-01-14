94b9d3e20084b2265d738a04aeaaeaf37852fffb
Ross Leitch <ross@hexhive.io>
2024-01-15 12:35:50 +1300

    Inlet filter

--- dist/UV.js
+++ dist/UV.js
@@ -23,68 +23,63 @@ var __importStar = (this && this.__importStar) || function (mod) {
     return result;
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.UV = void 0;
 const React = __importStar(require("react"));
-const UV = (props) => (React.createElement("svg", Object.assign({ xmlns: "http://www.w3.org/2000/svg", xmlSpace: "preserve", id: "uv_svg__Layer_1", x: 0, y: 0, viewBox: "0 0 165.4 45.3" }, props),
-    React.createElement("style", null, ".uv_svg__st1{fill:#4d4d4e}"),
-    React.createElement("linearGradient", { id: "uv_svg__SVGID_1_", x1: 75.411, x2: 75.411, y1: 14.88, y2: 44.88, gradientUnits: "userSpaceOnUse" },
-        React.createElement("stop", { offset: 0, style: {
-                stopColor: "#393839",
-            } }),
-        React.createElement("stop", { offset: 0.5, style: {
-                stopColor: "#7e7f81",
-            } }),
-        React.createElement("stop", { offset: 1, style: {
-                stopColor: "#393839",
-            } })),
-    React.createElement("path", { d: "M146.9 44.9H3.9C2 44.9.4 43.3.4 41.4v-23c0-1.9 1.6-3.5 3.5-3.5h143c1.9 0 3.5 1.6 3.5 3.5v23c0 1.9-1.6 3.5-3.5 3.5z", style: {
-            fill: "url(#uv_svg__SVGID_1_)",
+const SvgUv = (props) => (React.createElement("svg", Object.assign({ xmlns: "http://www.w3.org/2000/svg", xmlnsXlink: "http://www.w3.org/1999/xlink", id: "uv_svg__Layer_1", "data-name": "Layer 1", viewBox: "0 0 165.4 45.3" }, props),
+    React.createElement("defs", null,
+        React.createElement("linearGradient", { id: "uv_svg__linear-gradient", x1: 75.4, x2: 75.4, y1: 33.1, y2: 3.1, gradientTransform: "matrix(1 0 0 -1 0 48)", gradientUnits: "userSpaceOnUse" },
+            React.createElement("stop", { offset: 0, stopColor: "#393839" }),
+            React.createElement("stop", { offset: 0.5, stopColor: "#7e7f81" }),
+            React.createElement("stop", { offset: 1, stopColor: "#393839" })),
+        React.createElement("linearGradient", { xlinkHref: "#uv_svg__linear-gradient", id: "uv_svg__linear-gradient-2", x1: 0.4, x2: 30.4, y1: 45.4, y2: 45.4 }),
+        React.createElement("linearGradient", { xlinkHref: "#uv_svg__linear-gradient", id: "uv_svg__linear-gradient-3", x1: 162.7, x2: 162.7, y1: 33.1, y2: 3.1 }),
+        React.createElement("linearGradient", { id: "uv_svg__no-flow", x1: 75.4, x2: 75.4, y1: 38.3, y2: 21.5, gradientUnits: "userSpaceOnUse" },
+            React.createElement("stop", { offset: 0, stopColor: "#7e7f81" }),
+            React.createElement("stop", { offset: 0.5, stopColor: "#717173" }),
+            React.createElement("stop", { offset: 1, stopColor: "#7e7f81" })),
+        React.createElement("linearGradient", { id: "uv_svg__uv-off", x1: 74, x2: 74, y1: 32.14, y2: 27.66, gradientUnits: "userSpaceOnUse" },
+            React.createElement("stop", { offset: 0, stopColor: "#7c7c7c" }),
+            React.createElement("stop", { offset: 0.5, stopColor: "#c9c9c9" }),
+            React.createElement("stop", { offset: 1, stopColor: "#7c7c7c" })),
+        React.createElement("linearGradient", { id: "uv_svg__uv-on", x1: 74, x2: 74, y1: 32.14, y2: 27.66, gradientUnits: "userSpaceOnUse" },
+            React.createElement("stop", { offset: 0, stopColor: "#4c48ff" }),
+            React.createElement("stop", { offset: 0.02, stopColor: "#5653ff" }),
+            React.createElement("stop", { offset: 0.11, stopColor: "#7371ff" }),
+            React.createElement("stop", { offset: 0.19, stopColor: "#8989ff" }),
+            React.createElement("stop", { offset: 0.28, stopColor: "#999aff" }),
+            React.createElement("stop", { offset: 0.38, stopColor: "#a2a4ff" }),
+            React.createElement("stop", { offset: 0.5, stopColor: "#a5a7ff" }),
+            React.createElement("stop", { offset: 0.62, stopColor: "#a2a4ff" }),
+            React.createElement("stop", { offset: 0.72, stopColor: "#999aff" }),
+            React.createElement("stop", { offset: 0.81, stopColor: "#8989ff" }),
+            React.createElement("stop", { offset: 0.9, stopColor: "#7371ff" }),
+            React.createElement("stop", { offset: 0.98, stopColor: "#5653ff" }),
+            React.createElement("stop", { offset: 1, stopColor: "#4c48ff" })),
+        React.createElement("linearGradient", { id: "uv_svg__flow-with-uv", x1: 75.45, x2: 75.45, y1: 38.3, y2: 21.5, gradientUnits: "userSpaceOnUse" },
+            React.createElement("stop", { offset: 0, stopColor: "#6aa8e5" }),
+            React.createElement("stop", { offset: 0.5, stopColor: "#fff" }),
+            React.createElement("stop", { offset: 1, stopColor: "#6aa8e5" })),
+        React.createElement("linearGradient", { id: "uv_svg__flow-with-no-uv" },
+            React.createElement("stop", { offset: 0, stopColor: "#6aa8e5" }),
+            ">"),
+        React.createElement("style", null, ".uv_svg__cls-2{fill:#4d4d4e}")),
+    React.createElement("path", { d: "M146.9 44.9H3.9a3.54 3.54 0 0 1-3.5-3.5v-23a3.54 3.54 0 0 1 3.5-3.5h143a3.54 3.54 0 0 1 3.5 3.5v23a3.54 3.54 0 0 1-3.5 3.5", style: {
+            fill: "url(#uv_svg__linear-gradient)",
         } }),
-    React.createElement("path", { d: "M6.27 14.902v-10h17.6v10z", className: "uv_svg__st1" }),
-    React.createElement("linearGradient", { id: "uv_svg__SVGID_00000114042839260797354350000001853898132272339639_", x1: 0.411, x2: 30.411, y1: 2.614, y2: 2.614, gradientUnits: "userSpaceOnUse" },
-        React.createElement("stop", { offset: 0, style: {
-                stopColor: "#393839",
-            } }),
-        React.createElement("stop", { offset: 0.5, style: {
-                stopColor: "#7e7f81",
-            } }),
-        React.createElement("stop", { offset: 1, style: {
-                stopColor: "#393839",
-            } })),
-    React.createElement("path", { d: "M30.4 2.6c0-1.2-1-2.2-2.2-2.2H2.6C1.4.4.4 1.4.4 2.6c0 1.2 1 2.2 2.2 2.2h25.5c1.3.1 2.3-.9 2.3-2.2z", style: {
-            fill: "url(#uv_svg__SVGID_00000114042839260797354350000001853898132272339639_)",
+    React.createElement("path", { d: "M6.27 4.9h17.6v10H6.27z", className: "uv_svg__cls-2" }),
+    React.createElement("path", { d: "M30.4 2.6A2.22 2.22 0 0 0 28.2.4H2.6A2.22 2.22 0 0 0 .4 2.6a2.22 2.22 0 0 0 2.2 2.2h25.5a2.13 2.13 0 0 0 2.3-2.2", style: {
+            fill: "url(#uv_svg__linear-gradient-2)",
         } }),
-    React.createElement("path", { d: "M150.4 20.7h10v17.6h-10z", className: "uv_svg__st1" }),
-    React.createElement("linearGradient", { id: "uv_svg__SVGID_00000170243620128869902510000018192573999936894911_", x1: 162.677, x2: 162.677, y1: 14.88, y2: 44.88, gradientUnits: "userSpaceOnUse" },
-        React.createElement("stop", { offset: 0, style: {
-                stopColor: "#393839",
-            } }),
-        React.createElement("stop", { offset: 0.5, style: {
-                stopColor: "#7e7f81",
-            } }),
-        React.createElement("stop", { offset: 1, style: {
-                stopColor: "#393839",
-            } })),
-    React.createElement("path", { d: "M162.7 44.9c1.2 0 2.2-1 2.2-2.2V17.1c0-1.2-1-2.2-2.2-2.2-1.2 0-2.2 1-2.2 2.2v25.5c-.1 1.3.9 2.3 2.2 2.3z", style: {
-            fill: "url(#uv_svg__SVGID_00000170243620128869902510000018192573999936894911_)",
+    React.createElement("path", { d: "M150.4 20.7h10v17.6h-10z", className: "uv_svg__cls-2" }),
+    React.createElement("path", { d: "M162.7 44.9a2.22 2.22 0 0 0 2.2-2.2V17.1a2.22 2.22 0 0 0-2.2-2.2 2.22 2.22 0 0 0-2.2 2.2v25.5a2.13 2.13 0 0 0 2.2 2.3", style: {
+            fill: "url(#uv_svg__linear-gradient-3)",
+        } }),
+    React.createElement("rect", { width: 54.04, height: 16.8, x: 48.38, y: 21.5, rx: 3.25, style: {
+            fill: "url(#uv_svg__flow-with-no-uv)",
+        } }),
+    React.createElement("path", { d: "M56 27.66h38.28v4.49H56a2.24 2.24 0 0 1-2.24-2.24A2.24 2.24 0 0 1 56 27.66", style: {
+            fill: "url(#uv_svg__uv-off)",
+        } }),
+    React.createElement("path", { d: "M102.52 28.85H100A2.31 2.31 0 0 0 97.54 27h-3.26v5.9h3.26A2.31 2.31 0 0 0 100 31h2.49Z", style: {
+            fill: "#393839",
         } })));
-exports.UV = UV;
-exports.UV.metadata = {
-    type: 'uv',
-    width: 150,
-    height: 50,
-    ports: [
-        {
-            key: 'inlet',
-            momentum: 1,
-            x: '5%',
-            y: '5%',
-        },
-        {
-            key: 'outlet',
-            momentum: -1,
-            x: '95%',
-            y: '55%'
-        }
-    ]
-};
+exports.default = SvgUv;
--- src/UV.tsx
+++ src/UV.tsx
@@ -1,138 +1,150 @@
 import * as React from "react";
 import type { SVGProps } from "react";
-
-export const UV = (props: SVGProps<SVGSVGElement>) => (
+const SvgUv = (props: SVGProps<SVGSVGElement>) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
-    xmlSpace="preserve"
+    xmlnsXlink="http://www.w3.org/1999/xlink"
     id="uv_svg__Layer_1"
-    x={0}
-    y={0}
+    data-name="Layer 1"
     viewBox="0 0 165.4 45.3"
     {...props}
   >
-    <style>{".uv_svg__st1{fill:#4d4d4e}"}</style>
-    <linearGradient
-      id="uv_svg__SVGID_1_"
-      x1={75.411}
-      x2={75.411}
-      y1={14.88}
-      y2={44.88}
-      gradientUnits="userSpaceOnUse"
-    >
-      <stop
-        offset={0}
-        style={{
-          stopColor: "#393839",
-        }}
+    <defs>
+      <linearGradient
+        id="uv_svg__linear-gradient"
+        x1={75.4}
+        x2={75.4}
+        y1={33.1}
+        y2={3.1}
+        gradientTransform="matrix(1 0 0 -1 0 48)"
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop offset={0} stopColor="#393839" />
+        <stop offset={0.5} stopColor="#7e7f81" />
+        <stop offset={1} stopColor="#393839" />
+      </linearGradient>
+      <linearGradient
+        xlinkHref="#uv_svg__linear-gradient"
+        id="uv_svg__linear-gradient-2"
+        x1={0.4}
+        x2={30.4}
+        y1={45.4}
+        y2={45.4}
       />
-      <stop
-        offset={0.5}
-        style={{
-          stopColor: "#7e7f81",
-        }}
+      <linearGradient
+        xlinkHref="#uv_svg__linear-gradient"
+        id="uv_svg__linear-gradient-3"
+        x1={162.7}
+        x2={162.7}
+        y1={33.1}
+        y2={3.1}
       />
-      <stop
-        offset={1}
-        style={{
-          stopColor: "#393839",
-        }}
-      />
-    </linearGradient>
+      <linearGradient
+        id="uv_svg__no-flow"
+        x1={75.4}
+        x2={75.4}
+        y1={38.3}
+        y2={21.5}
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop offset={0} stopColor="#7e7f81" />
+        <stop offset={0.5} stopColor="#717173" />
+        <stop offset={1} stopColor="#7e7f81" />
+      </linearGradient>
+      <linearGradient
+        id="uv_svg__uv-off"
+        x1={74}
+        x2={74}
+        y1={32.14}
+        y2={27.66}
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop offset={0} stopColor="#7c7c7c" />
+        <stop offset={0.5} stopColor="#c9c9c9" />
+        <stop offset={1} stopColor="#7c7c7c" />
+      </linearGradient>
+      <linearGradient
+        id="uv_svg__uv-on"
+        x1={74}
+        x2={74}
+        y1={32.14}
+        y2={27.66}
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop offset={0} stopColor="#4c48ff" />
+        <stop offset={0.02} stopColor="#5653ff" />
+        <stop offset={0.11} stopColor="#7371ff" />
+        <stop offset={0.19} stopColor="#8989ff" />
+        <stop offset={0.28} stopColor="#999aff" />
+        <stop offset={0.38} stopColor="#a2a4ff" />
+        <stop offset={0.5} stopColor="#a5a7ff" />
+        <stop offset={0.62} stopColor="#a2a4ff" />
+        <stop offset={0.72} stopColor="#999aff" />
+        <stop offset={0.81} stopColor="#8989ff" />
+        <stop offset={0.9} stopColor="#7371ff" />
+        <stop offset={0.98} stopColor="#5653ff" />
+        <stop offset={1} stopColor="#4c48ff" />
+      </linearGradient>
+      <linearGradient
+        id="uv_svg__flow-with-uv"
+        x1={75.45}
+        x2={75.45}
+        y1={38.3}
+        y2={21.5}
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop offset={0} stopColor="#6aa8e5" />
+        <stop offset={0.5} stopColor="#fff" />
+        <stop offset={1} stopColor="#6aa8e5" />
+      </linearGradient>
+      <linearGradient id="uv_svg__flow-with-no-uv">
+        <stop offset={0} stopColor="#6aa8e5" />
+        {">"}
+      </linearGradient>
+      <style>{".uv_svg__cls-2{fill:#4d4d4e}"}</style>
+    </defs>
     <path
-      d="M146.9 44.9H3.9C2 44.9.4 43.3.4 41.4v-23c0-1.9 1.6-3.5 3.5-3.5h143c1.9 0 3.5 1.6 3.5 3.5v23c0 1.9-1.6 3.5-3.5 3.5z"
+      d="M146.9 44.9H3.9a3.54 3.54 0 0 1-3.5-3.5v-23a3.54 3.54 0 0 1 3.5-3.5h143a3.54 3.54 0 0 1 3.5 3.5v23a3.54 3.54 0 0 1-3.5 3.5"
       style={{
-        fill: "url(#uv_svg__SVGID_1_)",
+        fill: "url(#uv_svg__linear-gradient)",
       }}
     />
-    <path d="M6.27 14.902v-10h17.6v10z" className="uv_svg__st1" />
-    <linearGradient
-      id="uv_svg__SVGID_00000114042839260797354350000001853898132272339639_"
-      x1={0.411}
-      x2={30.411}
-      y1={2.614}
-      y2={2.614}
-      gradientUnits="userSpaceOnUse"
-    >
-      <stop
-        offset={0}
-        style={{
-          stopColor: "#393839",
-        }}
-      />
-      <stop
-        offset={0.5}
-        style={{
-          stopColor: "#7e7f81",
-        }}
-      />
-      <stop
-        offset={1}
-        style={{
-          stopColor: "#393839",
-        }}
-      />
-    </linearGradient>
+    <path d="M6.27 4.9h17.6v10H6.27z" className="uv_svg__cls-2" />
     <path
-      d="M30.4 2.6c0-1.2-1-2.2-2.2-2.2H2.6C1.4.4.4 1.4.4 2.6c0 1.2 1 2.2 2.2 2.2h25.5c1.3.1 2.3-.9 2.3-2.2z"
+      d="M30.4 2.6A2.22 2.22 0 0 0 28.2.4H2.6A2.22 2.22 0 0 0 .4 2.6a2.22 2.22 0 0 0 2.2 2.2h25.5a2.13 2.13 0 0 0 2.3-2.2"
       style={{
-        fill: "url(#uv_svg__SVGID_00000114042839260797354350000001853898132272339639_)",
+        fill: "url(#uv_svg__linear-gradient-2)",
       }}
     />
-    <path d="M150.4 20.7h10v17.6h-10z" className="uv_svg__st1" />
-    <linearGradient
-      id="uv_svg__SVGID_00000170243620128869902510000018192573999936894911_"
-      x1={162.677}
-      x2={162.677}
-      y1={14.88}
-      y2={44.88}
-      gradientUnits="userSpaceOnUse"
-    >
-      <stop
-        offset={0}
-        style={{
-          stopColor: "#393839",
-        }}
-      />
-      <stop
-        offset={0.5}
-        style={{
-          stopColor: "#7e7f81",
-        }}
-      />
-      <stop
-        offset={1}
-        style={{
-          stopColor: "#393839",
-        }}
-      />
-    </linearGradient>
+    <path d="M150.4 20.7h10v17.6h-10z" className="uv_svg__cls-2" />
     <path
-      d="M162.7 44.9c1.2 0 2.2-1 2.2-2.2V17.1c0-1.2-1-2.2-2.2-2.2-1.2 0-2.2 1-2.2 2.2v25.5c-.1 1.3.9 2.3 2.2 2.3z"
+      d="M162.7 44.9a2.22 2.22 0 0 0 2.2-2.2V17.1a2.22 2.22 0 0 0-2.2-2.2 2.22 2.22 0 0 0-2.2 2.2v25.5a2.13 2.13 0 0 0 2.2 2.3"
       style={{
-        fill: "url(#uv_svg__SVGID_00000170243620128869902510000018192573999936894911_)",
+        fill: "url(#uv_svg__linear-gradient-3)",
+      }}
+    />
+    <rect
+      width={54.04}
+      height={16.8}
+      x={48.38}
+      y={21.5}
+      rx={3.25}
+      style={{
+        fill: "url(#uv_svg__flow-with-no-uv)",
+      }}
+    />
+    <path
+      d="M56 27.66h38.28v4.49H56a2.24 2.24 0 0 1-2.24-2.24A2.24 2.24 0 0 1 56 27.66"
+      style={{
+        fill: "url(#uv_svg__uv-off)",
+      }}
+    />
+    <path
+      d="M102.52 28.85H100A2.31 2.31 0 0 0 97.54 27h-3.26v5.9h3.26A2.31 2.31 0 0 0 100 31h2.49Z"
+      style={{
+        fill: "#393839",
       }}
     />
   </svg>
 );
-
-
-UV.metadata = {
-  type: 'uv',
-  width: 150,
-  height: 50,
-  ports: [
-    {
-      key: 'inlet',
-      momentum: 1,
-      x: '5%',
-      y: '5%',
-    },
-    {
-      key: 'outlet',
-      momentum: -1,
-      x: '95%',
-      y: '55%'
-    }
-  ]
-}
+export default SvgUv;
