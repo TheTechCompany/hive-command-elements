ef20383237df53d3c20a48650cd0244059cea734
Ross Leitch <ross@hexhive.io>
2024-01-15 13:21:09 +1300

    Inlet filter

--- dist/Multimedia.js
+++ dist/Multimedia.js
@@ -25,10 +25,75 @@ var __importStar = (this && this.__importStar) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.Multimedia = void 0;
 const React = __importStar(require("react"));
-const Multimedia = (props) => (React.createElement("svg", Object.assign({ xmlns: "http://www.w3.org/2000/svg", xmlSpace: "preserve", id: "multimedia_svg__Layer_1", x: 0, y: 0, viewBox: "0 0 31.5 97.4" }, props),
-    React.createElement("style", null, ".multimedia_svg__st0{fill:#4d4d4e}"),
-    React.createElement("path", { d: "M10 19.6c1.3 1 3.3 1.5 5.7 1.5 2.4 0 4.4-.6 5.7-1.5 3.8 2.1 6.3 6.1 6.3 10.6v62.5c0 .6-.5 1.1-1.1 1.1H4.8c-.6 0-1.1-.5-1.1-1.1V30.1c0-4.4 2.5-8.5 6.3-10.5m10.4-3.7c-.2 1.5-2.4 2.2-4.7 2.2-2.3 0-4.5-.7-4.7-2.2C5 17.9.7 23.5.7 30.1v62.5c0 2.3 1.8 4.1 4.1 4.1h21.8c2.3 0 4.1-1.8 4.1-4.1V30.1c0-6.6-4.3-12.2-10.3-14.2z", className: "multimedia_svg__st0" }),
-    React.createElement("path", { d: "M27.7 3.6v2.2h-8.3c-1.7 0-3 1.3-3 3v4.3H15V8.8c0-1.7-1.3-3-3-3H3.7V3.6h24m.6-3H3.1C1.8.6.7 1.7.7 3v3.4c0 1.3 1.1 2.4 2.4 2.4H12v5.9c0 .8 1.7 1.4 3.7 1.4s3.7-.6 3.7-1.4V8.8h8.9c1.3 0 2.4-1.1 2.4-2.4V3c0-1.3-1.1-2.4-2.4-2.4z", className: "multimedia_svg__st0" })));
+const Multimedia = (props) => {
+    var _a;
+    return (React.createElement("svg", Object.assign({ xmlns: "http://www.w3.org/2000/svg", xmlSpace: "preserve", id: "Multimedia_svg__Layer_1", x: 0, y: 0, viewBox: "0 0 31.5 97.4" }, props),
+        React.createElement("style", null, `
+      .Multimedia_svg__st1{
+        fill:#393839;
+      }
+      
+      .Multimedia_svg__tankHead{
+        fill:${((_a = props.options) === null || _a === void 0 ? void 0 : _a.headFill) || '#393839'};
+      }`),
+        React.createElement("linearGradient", { id: "Multimedia_svg__SVGID_1_", x1: 1.218, x2: 30.153, y1: 48.7, y2: 48.7, gradientUnits: "userSpaceOnUse" },
+            React.createElement("stop", { offset: 0, style: {
+                    stopColor: "#393839",
+                } }),
+            React.createElement("stop", { offset: 0.5, style: {
+                    stopColor: "#7e7f81",
+                } }),
+            React.createElement("stop", { offset: 1, style: {
+                    stopColor: "#393839",
+                } })),
+        React.createElement("path", { d: "M15.7 15.2c-8 0-14.5 6.5-14.5 14.5V78.1c0 2.8 6.6 4.1 14.6 4.1s14.3-1.3 14.3-4.1V29.7c.1-8-6.4-14.5-14.4-14.5", style: {
+                fill: "url(#Multimedia_svg__SVGID_1_)",
+            } }),
+        React.createElement("path", { d: "M29 1.9H2.5c-.7 0-1.3.6-1.3 1.3v5c0 .7.6 1.3 1.3 1.3h9.7v4c0 2 1.6 2.8 3.6 2.8s3.6-.8 3.6-2.8V9.4H29c.7 0 1.3-.6 1.3-1.3v-5c0-.6-.6-1.2-1.3-1.2", className: "Multimedia_svg__tankHead" }),
+        React.createElement("path", { d: "M19.2 16.2h-6.9c-.5 0-.9-.4-.9-.9s.4-.9.9-.9h6.9c.5 0 .9.4.9.9.1.5-.3.9-.9.9", style: {
+                fill: "#a8a8a8",
+            } }),
+        React.createElement("linearGradient", { id: "Multimedia_svg__SVGID_2_", x1: 93.49, x2: 170.696, y1: 133.917, y2: 133.917, gradientUnits: "userSpaceOnUse" },
+            React.createElement("stop", { offset: 0, style: {
+                    stopColor: "#393839",
+                } }),
+            React.createElement("stop", { offset: 0.5, style: {
+                    stopColor: "#7e7f81",
+                } }),
+            React.createElement("stop", { offset: 1, style: {
+                    stopColor: "#393839",
+                } })),
+        React.createElement("path", { d: "M93.5 133v1.7c0-.3 0-.5.1-.8-.1-.2-.1-.5-.1-.9", style: {
+                fill: "url(#Multimedia_svg__SVGID_2_)",
+            } }),
+        React.createElement("linearGradient", { id: "Multimedia_svg__SVGID_3_", x1: 74.749, x2: 193.436, y1: 133.917, y2: 133.917, gradientUnits: "userSpaceOnUse" },
+            React.createElement("stop", { offset: 0, style: {
+                    stopColor: "#393839",
+                } }),
+            React.createElement("stop", { offset: 0.5, style: {
+                    stopColor: "#7e7f81",
+                } }),
+            React.createElement("stop", { offset: 1, style: {
+                    stopColor: "#393839",
+                } })),
+        React.createElement("path", { d: "M93.5 133v1.7c0-.3 0-.5.1-.8-.1-.2-.1-.5-.1-.9", style: {
+                fill: "url(#Multimedia_svg__SVGID_3_)",
+            } }),
+        React.createElement("linearGradient", { id: "Multimedia_svg__SVGID_4_", x1: 1.218, x2: 30.153, y1: 86.815, y2: 86.815, gradientUnits: "userSpaceOnUse" },
+            React.createElement("stop", { offset: 0, style: {
+                    stopColor: "#393839",
+                } }),
+            React.createElement("stop", { offset: 0.5, style: {
+                    stopColor: "#7e7f81",
+                } }),
+            React.createElement("stop", { offset: 1, style: {
+                    stopColor: "#393839",
+                } })),
+        React.createElement("path", { d: "M15.8 82.2c-8 0-14.6-1.3-14.6-4.1v13.4c0 2.8 6.6 4.1 14.6 4.1s14.3-1.3 14.3-4.1V78.1c.1 2.8-6.3 4.1-14.3 4.1", style: {
+                fill: "url(#Multimedia_svg__SVGID_4_)",
+            } }),
+        React.createElement("path", { d: "M15.8 82.2c-8 0-14.6-1.3-14.6-4.1v13.4c0 2.8 6.6 4.1 14.6 4.1s14.3-1.3 14.3-4.1V78.1c.1 2.8-6.3 4.1-14.3 4.1", className: "Multimedia_svg__st1" })));
+};
 exports.Multimedia = Multimedia;
 exports.Multimedia.metadata = {
     type: 'multimedia',
